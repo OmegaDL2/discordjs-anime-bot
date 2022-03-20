0607428a41cbe1701cb6709679f489751face333
Omega-Laptop <noreply@github.com>
2022-03-20 19:23:32 +0100

    comment correction

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client } = new require("discord.js");
 const client = new Client({intents: 32767});
 require('dotenv').config();
 
-module.exports = {   // Function to get the day/month/year --- hour:minute:second.millisecond | Unix time    // how to use: var tools = require('./index');console.log(tools.time());           
+module.exports = {   // Function to get the [day/month/year --- hour:minute:second.millisecond | Unix time]   // how to use: var tools = require('./index');console.log(tools.time());           
     GetTime: function () {
         var Time = new Date();
         return "     [" + Time.getDate() + "/" + (Time.getMonth() + 1) + "/" + Time.getFullYear() + " --- " + Time.getHours() + ":" + Time.getMinutes() + ":" + Time.getSeconds() + "." + Time.getMilliseconds() + " | " + Time.getTime() + "]";    
