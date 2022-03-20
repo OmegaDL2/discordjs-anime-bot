5d6c2f2d634a9e8ead6870c8fb2d8b42dd25b167
Omega-Desktop <noreply@github.com>
2022-03-20 10:58:35 +0100

    added unix time

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { Client } = new require("discord.js");
 const client = new Client({intents: 32767});
 require('dotenv').config();
 
-function GetTime(){     // Function to get the day/month/year --- hour:minute:second.millisecond
+function GetTime(){     // Function to get the day/month/year --- hour:minute:second.millisecond | Unix time
     var Time = new Date();
-    return Time.getDate() + "/" + (Time.getMonth() + 1) + "/" + Time.getFullYear() + " --- " + Time.getHours() + ":" + Time.getMinutes() + ":" + Time.getSeconds() + "." + Time.getMilliseconds();
+    return Time.getDate() + "/" + (Time.getMonth() + 1) + "/" + Time.getFullYear() + " --- " + Time.getHours() + ":" + Time.getMinutes() + ":" + Time.getSeconds() + "." + Time.getMilliseconds() + " | " + Time.getTime();
 };
 
 require("./Handlers/Events")(client);
